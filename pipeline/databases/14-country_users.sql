29992555f01dc4d6f0c097f6a5204058f6a353e7
Rochelle Rothman <rothman.rochelle@gmail.com>
2026-05-31 22:13:37 +0200

    Create users table with country enum

---
+++ pipeline/databases/14-country_users.sql
@@ -0,0 +1,8 @@
+-- Creates the users table with a country enumeration
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+    PRIMARY KEY (id)
+);
