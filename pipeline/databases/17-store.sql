7448ae356fbe81674f054907af4380e8e41930cd
Rochelle Rothman <rothman.rochelle@gmail.com>
2026-05-31 22:26:38 +0200

    Create order quantity trigger

---
+++ pipeline/databases/17-store.sql
@@ -0,0 +1,13 @@
+-- Creates a trigger that decreases item quantity after a new order
+DELIMITER $$
+
+CREATE TRIGGER decrease_item_quantity
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END$$
+
+DELIMITER ;
