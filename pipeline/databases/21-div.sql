5f42a2d2d5776285028a6808d4066f535d7073f6
Rochelle Rothman <rothman.rochelle@gmail.com>
2026-05-31 22:31:58 +0200

    Create safe division function

---
+++ pipeline/databases/21-div.sql
@@ -0,0 +1,15 @@
+-- Creates a function that safely divides two numbers
+DELIMITER $$
+
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    END IF;
+
+    RETURN a / b;
+END$$
+
+DELIMITER ;
