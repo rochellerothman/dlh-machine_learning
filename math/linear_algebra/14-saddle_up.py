2adb42679439ffe4d19ebff5cf81063228173430
RochelleRothman <Rothman.Rochelle@gmail.com>
2026-06-15 21:34:34 +0200

    Add numpy matrix multiplication function

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module that contains a function for matrix multiplication."""
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Performs matrix multiplication."""
+    return np.matmul(mat1, mat2)
