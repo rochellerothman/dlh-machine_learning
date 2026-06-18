e41464c504d6ba76f3606584d9baf2ac34cc52d4
RochelleRothman <Rothman.Rochelle@gmail.com>
2026-06-18 14:13:03 +0000

    Add cofactor matrix calculation

---
+++ math/advanced_linear_algebra/2-cofactor.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+"""Module that calculates the cofactor matrix of a matrix."""
+
+
+def determinant(matrix):
+    """Calculates the determinant of a matrix."""
+
+    if matrix == [[]]:
+        return 1
+
+    if len(matrix) == 1:
+        return matrix[0][0]
+
+    if len(matrix) == 2:
+        return matrix[0][0] * matrix[1][1] - \
+            matrix[0][1] * matrix[1][0]
+
+    det = 0
+
+    for col in range(len(matrix)):
+        sub = [row[:col] + row[col + 1:] for row in matrix[1:]]
+        det += ((-1) ** col) * matrix[0][col] * determinant(sub)
+
+    return det
+
+
+def cofactor(matrix):
+    """Calculates the cofactor matrix of a matrix."""
+
+    if not isinstance(matrix, list) or matrix == []:
+        raise TypeError("matrix must be a list of lists")
+
+    if not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a list of lists")
+
+    n = len(matrix)
+
+    if not all(len(row) == n for row in matrix):
+        raise ValueError("matrix must be a non-empty square matrix")
+
+    if n == 1:
+        return [[1]]
+
+    cof = []
+
+    for i in range(n):
+        row = []
+
+        for j in range(n):
+            sub = [
+                r[:j] + r[j + 1:]
+                for k, r in enumerate(matrix)
+                if k != i
+            ]
+
+            sign = (-1) ** (i + j)
+            row.append(sign * determinant(sub))
+
+        cof.append(row)
+
+    return cof
