219fc00e524b237970ffa836494213d596a2ab5c
RochelleRothman <Rothman.Rochelle@gmail.com>
2026-07-04 11:13:47 +0200

    Fix task 4 histogram ticks

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -11,10 +11,13 @@ def frequency():
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
 
-    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+    plt.hist(student_grades, bins=np.arange(0, 101, 10),
+             edgecolor='black')
     plt.xlabel("Grades")
     plt.ylabel("Number of Students")
     plt.title("Project A")
+    plt.xticks(np.arange(0, 101, 10))
+    plt.yticks(np.arange(0, 31, 5))
     plt.xlim(0, 100)
     plt.ylim(0, 30)
 
