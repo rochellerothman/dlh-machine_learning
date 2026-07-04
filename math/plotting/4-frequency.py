53ff2695b116844f74efbd00ff09c84bf0bfabf5
RochelleRothman <Rothman.Rochelle@gmail.com>
2026-07-04 10:58:49 +0200

    Fix frequency plot

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -9,18 +9,12 @@ def frequency():
     """Plot a histogram of student grades."""
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
-
     plt.figure(figsize=(6.4, 4.8))
 
-    plt.hist(student_grades,
-             bins=np.arange(0, 101, 10),
-             edgecolor='black')
+    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
 
-    plt.title("Project A")
     plt.xlabel("Grades")
     plt.ylabel("Number of Students")
-
-    plt.xlim(0, 100)
-    plt.ylim(0, 30)
+    plt.title("Project A")
 
     plt.show()
